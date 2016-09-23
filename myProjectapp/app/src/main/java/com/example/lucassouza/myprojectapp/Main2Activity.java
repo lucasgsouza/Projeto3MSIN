9f9a2793e7dfe44cd2bdc8e147047ebf0caeb5f0
Lucas Souza <lucas.gomesdesouza@gmail.com>
2016-09-23 17:17:08 -0300

    Atualizando ListView

--- myProjectapp/app/src/main/java/com/example/lucassouza/myprojectapp/Main2Activity.java
+++ myProjectapp/app/src/main/java/com/example/lucassouza/myprojectapp/Main2Activity.java
@@ -6,13 +6,55 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+
+
+        String cursos[] = new String[]{"Arte Contemporânea","Java Básico","Redes","oi"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, cursos);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
     }
 
 }
